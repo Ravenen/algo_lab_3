5f06813f0ba4ec68634d521a255c8f5ea5a39ac9
Raven <redstoyningbros@gmail.com>
2020-11-09 14:49:45 +0200

    Added type annotation for Vertex

--- graph/vertex.py
+++ graph/vertex.py
@@ -1,5 +1,5 @@
 class Vertex(object):
 
-    def __init__(self, index, is_client):
+    def __init__(self, index: int, is_client: bool):
         self.index = index
         self.is_client = is_client
