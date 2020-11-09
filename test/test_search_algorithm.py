069d79a427210284633bcb61f94e9cf16effc5ef
Raven <redstoyningbros@gmail.com>
2020-11-09 20:34:59 +0200

    Added tests for searching algorithm

---
+++ test/test_search_algorithm.py
@@ -0,0 +1,31 @@
+import unittest
+import copy
+
+from graph.graph import Graph
+from graph.search_algorithm import search_shortest_paths_from
+
+
+class TestSearchAlgorithm(unittest.TestCase):
+
+    def setUp(self) -> None:
+        self.first_graph = Graph()
+        self.first_graph.add_edges_from_vertexes(1, 2, 10)
+        self.first_graph.add_edges_from_vertexes(1, 5, 70)
+        self.first_graph.add_edges_from_vertexes(2, 3, 20)
+        self.first_graph.add_edges_from_vertexes(3, 4, 20)
+        self.first_graph.add_edges_from_vertexes(4, 5, 10)
+        self.first_graph.add_edges_from_vertexes(5, 6, 10)
+
+    def test_search_shortest_paths_from_first(self):
+        shortest_paths = search_shortest_paths_from(1, self.first_graph)
+        self.assertEqual(shortest_paths[5], 60)
+
+    def test_search_shortest_paths_from_second(self):
+        shortest_paths = search_shortest_paths_from(1, self.first_graph)
+        self.assertEqual(shortest_paths[6], 70)
+
+    def test_search_shortest_paths_from_third(self):
+        second_graph = copy.deepcopy(self.first_graph)
+        second_graph.add_edges_from_vertexes(1, 6, 40)
+        shortest_paths = search_shortest_paths_from(1, second_graph)
+        self.assertEqual(shortest_paths[5], 50)
