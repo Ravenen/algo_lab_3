6d73c370c86341328a5a4b38935b6ef0275fe8ad
Raven <redstoyningbros@gmail.com>
2020-11-09 17:32:50 +0200

    Edited algorithm to use adjacency_list

--- graph/search_algorithm.py
+++ graph/search_algorithm.py
@@ -1,21 +1,21 @@
 from heapq import heappush, heappop
-from typing import Dict
+from typing import Dict, List, Tuple
 
 from graph.graph import Graph
 
 
 def search_shortest_paths_from(start_index: int, graph: Graph) -> Dict[int, int]:
-    return dijkstra_algorithm(start_index, graph)
+    return dijkstra_algorithm(start_index, graph.adjacencies)
 
 
-def dijkstra_algorithm(base_index: int, graph: Graph) -> Dict[int, int]:
+def dijkstra_algorithm(base_index: int, adjacency_list: Dict[int, List[Tuple[int, int]]]) -> Dict[int, int]:
     queue = []
     heappush(queue, (0, base_index))
     weights = {base_index: 0}
 
     while queue:
         current_weight, current_vertex_index = heappop(queue)
-        neighbour_vertexes = graph.adjacencies[current_vertex_index]
+        neighbour_vertexes = adjacency_list[current_vertex_index]
         for neighbour_vertex in neighbour_vertexes:
             weight, vertex_index = neighbour_vertex
             total_weight = current_weight + weight
