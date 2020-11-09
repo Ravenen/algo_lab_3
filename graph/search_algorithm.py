85a40b2246782dbe39e7af3d450b7c0d6e7ffdcf
Raven <redstoyningbros@gmail.com>
2020-11-09 18:27:22 +0200

    Renamed variables

--- graph/search_algorithm.py
+++ graph/search_algorithm.py
@@ -16,11 +16,12 @@ def dijkstra_algorithm(base_index: int, adjacency_list: Dict[int, List[Tuple[int
     while queue:
         current_weight, current_vertex_index = heappop(queue)
         neighbour_vertexes = adjacency_list[current_vertex_index]
+
         for neighbour_vertex in neighbour_vertexes:
-            weight, vertex_index = neighbour_vertex
-            total_weight = current_weight + weight
-            if vertex_index not in weights.keys() or total_weight < weights[vertex_index]:
-                weights[vertex_index] = total_weight
-                heappush(queue, (total_weight, vertex_index))
+            neighbour_edge_weight, neighbour_vertex_index = neighbour_vertex
+            total_path_weight = current_weight + neighbour_edge_weight
+            if neighbour_vertex_index not in weights.keys() or total_path_weight < weights[neighbour_vertex_index]:
+                weights[neighbour_vertex_index] = total_path_weight
+                heappush(queue, (total_path_weight, neighbour_vertex_index))
 
     return weights
