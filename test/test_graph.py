b7b701e7c0414f02717d8a295fb993bdc929856a
Raven <redstoyningbros@gmail.com>
2020-11-19 13:44:57 +0200

    Edited test: swapped expected and actual values and added Edge class support

--- test/test_graph.py
+++ test/test_graph.py
@@ -1,4 +1,6 @@
 import unittest
+
+from graph.edge import Edge
 from graph.graph import Graph
 
 
@@ -6,49 +8,55 @@ class TestGraph(unittest.TestCase):
 
     def setUp(self) -> None:
         self.first_graph = Graph()
-        self.first_graph.add_edges_from_vertexes(1, 2, 5)
-        self.first_graph.add_edges_from_vertexes(2, 3, 5)
-        self.first_graph.add_edges_from_vertexes(1, 3, 15)
+        self.first_graph.add_bidirectional_edge_from_vertexes(1, 2, 5)
+        self.first_graph.add_bidirectional_edge_from_vertexes(2, 3, 5)
+        self.first_graph.add_bidirectional_edge_from_vertexes(1, 3, 15)
 
         self.second_graph = Graph()
-        self.second_graph.add_edges_from_vertexes(1, 2, 50)
-        self.second_graph.add_edges_from_vertexes(1, 3, 70)
-        self.second_graph.add_edges_from_vertexes(3, 2, 80)
-        self.second_graph.add_edges_from_vertexes(3, 4, 50)
-        self.second_graph.add_edges_from_vertexes(5, 3, 60)
+        self.second_graph.add_bidirectional_edge_from_vertexes(1, 2, 50)
+        self.second_graph.add_bidirectional_edge_from_vertexes(1, 3, 70)
+        self.second_graph.add_bidirectional_edge_from_vertexes(3, 2, 80)
+        self.second_graph.add_bidirectional_edge_from_vertexes(3, 4, 50)
+        self.second_graph.add_bidirectional_edge_from_vertexes(5, 3, 60)
 
     def test_vertexes_quantity_first(self):
-        self.assertEqual(len(self.first_graph.get_vertexes()), 3)
+        self.assertEqual(3, len(self.first_graph.get_vertexes()))
 
     def test_vertexes_composition_first(self):
-        self.assertSetEqual(set(self.first_graph.get_vertexes()), {1, 2, 3})
+        self.assertSetEqual({1, 2, 3}, set(self.first_graph.get_vertexes()))
 
     def test_edges_quantity_first(self):
-        self.assertEqual(len(self.first_graph.adjacencies[1]), 2)
-        self.assertEqual(len(self.first_graph.adjacencies[2]), 2)
-        self.assertEqual(len(self.first_graph.adjacencies[3]), 2)
+        self.assertEqual(2, len(self.first_graph.adjacencies[1]))
+        self.assertEqual(2, len(self.first_graph.adjacencies[2]))
+        self.assertEqual(2, len(self.first_graph.adjacencies[3]))
 
     def test_edges_composition_first(self):
-        self.assertSetEqual(set(self.first_graph.adjacencies[1]), {(5, 2), (15, 3)})
-        self.assertSetEqual(set(self.first_graph.adjacencies[2]), {(5, 3), (5, 1)})
-        self.assertSetEqual(set(self.first_graph.adjacencies[3]), {(5, 2), (15, 1)})
+        self.assertSetEqual({Edge(2, 5), Edge(3, 15)},
+                            set(self.first_graph.adjacencies[1]))
+        self.assertSetEqual({Edge(3, 5), Edge(1, 5)},
+                            set(self.first_graph.adjacencies[2]))
+        self.assertSetEqual({Edge(2, 5), Edge(1, 15)},
+                            set(self.first_graph.adjacencies[3]))
 
     def test_vertexes_quantity_second(self):
-        self.assertEqual(len(self.second_graph.get_vertexes()), 5)
+        self.assertEqual(5, len(self.second_graph.get_vertexes()))
 
     def test_vertexes_composition_second(self):
-        self.assertSetEqual(set(self.second_graph.get_vertexes()), {1, 2, 3, 4, 5})
+        self.assertSetEqual({1, 2, 3, 4, 5}, set(self.second_graph.get_vertexes()))
 
     def test_edges_quantity_second(self):
-        self.assertEqual(len(self.second_graph.adjacencies[1]), 2)
-        self.assertEqual(len(self.second_graph.adjacencies[2]), 2)
-        self.assertEqual(len(self.second_graph.adjacencies[3]), 4)
-        self.assertEqual(len(self.second_graph.adjacencies[4]), 1)
-        self.assertEqual(len(self.second_graph.adjacencies[5]), 1)
+        self.assertEqual(2, len(self.second_graph.adjacencies[1]))
+        self.assertEqual(2, len(self.second_graph.adjacencies[2]))
+        self.assertEqual(4, len(self.second_graph.adjacencies[3]))
+        self.assertEqual(1, len(self.second_graph.adjacencies[4]))
+        self.assertEqual(1, len(self.second_graph.adjacencies[5]))
 
     def test_edges_composition_second(self):
-        self.assertSetEqual(set(self.second_graph.adjacencies[1]), {(50, 2), (70, 3)})
-        self.assertSetEqual(set(self.second_graph.adjacencies[2]), {(50, 1), (80, 3)})
-        self.assertSetEqual(set(self.second_graph.adjacencies[3]), {(70, 1), (80, 2), (50, 4), (60, 5)})
-        self.assertSetEqual(set(self.second_graph.adjacencies[4]), {(50, 3)})
-        self.assertSetEqual(set(self.second_graph.adjacencies[5]), {(60, 3)})
+        self.assertSetEqual({Edge(2, 50), Edge(3, 70)},
+                            set(self.second_graph.adjacencies[1]))
+        self.assertSetEqual({Edge(1, 50), Edge(3, 80)},
+                            set(self.second_graph.adjacencies[2]))
+        self.assertSetEqual({Edge(1, 70), Edge(2, 80), Edge(4, 50), Edge(5, 60)},
+                            set(self.second_graph.adjacencies[3]))
+        self.assertSetEqual({Edge(3, 50)}, set(self.second_graph.adjacencies[4]))
+        self.assertSetEqual({Edge(3, 60)}, set(self.second_graph.adjacencies[5]))
--- test/test_search_algorithm.py
+++ test/test_search_algorithm.py
@@ -9,23 +9,23 @@ class TestSearchAlgorithm(unittest.TestCase):
 
     def setUp(self) -> None:
         self.first_graph = Graph()
-        self.first_graph.add_edges_from_vertexes(1, 2, 10)
-        self.first_graph.add_edges_from_vertexes(1, 5, 70)
-        self.first_graph.add_edges_from_vertexes(2, 3, 20)
-        self.first_graph.add_edges_from_vertexes(3, 4, 20)
-        self.first_graph.add_edges_from_vertexes(4, 5, 10)
-        self.first_graph.add_edges_from_vertexes(5, 6, 10)
+        self.first_graph.add_bidirectional_edge_from_vertexes(1, 2, 10)
+        self.first_graph.add_bidirectional_edge_from_vertexes(1, 5, 70)
+        self.first_graph.add_bidirectional_edge_from_vertexes(2, 3, 20)
+        self.first_graph.add_bidirectional_edge_from_vertexes(3, 4, 20)
+        self.first_graph.add_bidirectional_edge_from_vertexes(4, 5, 10)
+        self.first_graph.add_bidirectional_edge_from_vertexes(5, 6, 10)
 
     def test_search_shortest_paths_from_first(self):
         shortest_paths = search_shortest_paths_from(1, self.first_graph)
-        self.assertEqual(shortest_paths[5], 60)
+        self.assertEqual(60, shortest_paths[5])
 
     def test_search_shortest_paths_from_second(self):
         shortest_paths = search_shortest_paths_from(1, self.first_graph)
-        self.assertEqual(shortest_paths[6], 70)
+        self.assertEqual(70, shortest_paths[6])
 
     def test_search_shortest_paths_from_third(self):
         second_graph = copy.deepcopy(self.first_graph)
-        second_graph.add_edges_from_vertexes(1, 6, 40)
+        second_graph.add_bidirectional_edge_from_vertexes(1, 6, 40)
         shortest_paths = search_shortest_paths_from(1, second_graph)
-        self.assertEqual(shortest_paths[5], 50)
+        self.assertEqual(50, shortest_paths[5])
